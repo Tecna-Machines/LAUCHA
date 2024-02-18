26d3bdf9815b3b011ea4d57cc2e34857cf040b35
Gaby <matiascoria427@gmail.com>
2024-02-17 23:39:38 -0300

    creada una clase para calcular porcentajes y agregados los Enums en la capa de dominio

---
+++ LAUCHA.application/Helpers/CalculadorDePorcentaje.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAUCHA.application.Helpers
+{
+    internal class CalculadorDePorcentaje
+    {
+        public decimal CalcularPorcentajeDeMonto(decimal porcentaje,decimal montoTotal)
+        {
+            decimal unPorCiento = montoTotal / 100;
+            return unPorCiento * porcentaje;
+        }
+
+        public decimal AumentarSegunPorcentaje(decimal porcentajeIncrementar,decimal montoOriginal)
+        {
+            // Calcular el incremento
+            decimal incremento = montoOriginal * (porcentajeIncrementar / 100);
+
+            // Sumar el incremento al monto original
+            decimal nuevoMonto = montoOriginal + incremento;
+
+            return nuevoMonto;
+        }
+    }
+}
---
+++ LAUCHA.domain/Enums/ModalidadContrato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAUCHA.domain.Enums
+{
+    public enum ModalidadContrato
+    {
+        mensualFijo = 10,
+        quincenalHora = 20,
+        quincenalFijo = 22,
+        mensualFijoHorasExtra = 12
+    }
+}
