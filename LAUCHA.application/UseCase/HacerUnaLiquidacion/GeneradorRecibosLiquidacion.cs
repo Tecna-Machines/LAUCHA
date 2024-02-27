bb8619fb1086becaa4968dee20193aa0640fe172
Gabyx708 <matiascoria427@gmail.com>
2024-02-27 10:47:08 -0300

    Refactorizada y pulida la logica para la crecion de recibos de sueldo

--- LAUCHA.application/UseCase/HacerUnaLiquidacion/GeneradorRecibosLiquidacion.cs
+++ LAUCHA.application/UseCase/HacerUnaLiquidacion/GeneradorRecibosLiquidacion.cs
@@ -1,11 +1,11 @@
-﻿using iText.IO.Font.Constants;
-using iText.Kernel.Colors;
-using iText.Kernel.Font;
+﻿using iText.Kernel.Colors;
 using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Draw;
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
+using LAUCHA.application.DTOs.DescuentoDTOs;
 using LAUCHA.application.DTOs.LiquidacionDTOs;
 using LAUCHA.application.DTOs.RemuneracionDTOs;
 using LAUCHA.application.DTOs.RetencionDTOs;
@@ -21,156 +21,197 @@ namespace LAUCHA.application.UseCase.HacerUnaLiquidacion
             {
                 PdfWriter writer = new PdfWriter(stream);
                 PdfDocument pdf = new PdfDocument(writer);
-                Document document = new Document(pdf,PageSize.A4);
-
-
-                // Crear encabezado con fondo gris
-                Table headerTable = new Table(1).UseAllAvailableWidth();
-                headerTable.SetBackgroundColor(ColorConstants.LIGHT_GRAY);
-
-                // Agregar celdas al encabezado
-                headerTable.AddCell(new Cell().Add(new Paragraph($"Fecha de Liquidación: {liquidacion.Fecha.ToShortDateString()}")));
-                headerTable.AddCell(new Cell().Add(new Paragraph($"Código de Liquidación: {liquidacion.Codigo}")));
-                headerTable.AddCell(new Cell().Add(new Paragraph($"Empleado: {liquidacion.Empleado}")));
-
-                document.Add(headerTable);
-                document.Add(new Paragraph("\n"));
+                Document document = new Document(pdf, PageSize.A4);
+                PageSize ps = pdf.GetDefaultPageSize();
+                document.SetFontSize(10);
 
 
                 // Crear tabla para la información
-                Table infoTable = new Table(2).UseAllAvailableWidth();
-
-                // Agregar información a la tabla
-                infoTable.AddCell("DNI del empleado:").AddCell(liquidacion.Dni);
-                infoTable.AddCell("Concepto:").AddCell(liquidacion.Concepto);
-
-                document.Add(infoTable);
+                Table header = this.GenerarTablaHeader(liquidacion);
+                document.Add(header);
 
 
-                // Agregar tabla de remuneraciones blancas
-                Table tablaRemuneracionesBlancas = new Table(4).UseAllAvailableWidth();
 
-                // Crear celdas de encabezado y establecer el color de fondo
-                Cell codigoHeaderCell = new Cell().Add(new Paragraph("CODIGO")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
-                Cell descripcionHeaderCell = new Cell().Add(new Paragraph("DESCRIPCION")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
-                Cell montoHeaderCell = new Cell().Add(new Paragraph("MONTO ($)")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
-                Cell fechaHeaderCell = new Cell().Add(new Paragraph("FECHA")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
-
-                // Agregar celdas de encabezado a la tabla
-                tablaRemuneracionesBlancas.AddHeaderCell(codigoHeaderCell);
-                tablaRemuneracionesBlancas.AddHeaderCell(descripcionHeaderCell);
-                tablaRemuneracionesBlancas.AddHeaderCell(montoHeaderCell);
-                tablaRemuneracionesBlancas.AddHeaderCell(fechaHeaderCell);
-
-                var listaRemuneracionBlancas = liquidacion.Items.Remuneraciones.Where(r => r.EsBlanco == true);
-
-                foreach (RemuneracionDTO remuneracion in listaRemuneracionBlancas)
-                {
-                    tablaRemuneracionesBlancas.AddCell(remuneracion.Codigo);
-                    tablaRemuneracionesBlancas.AddCell(remuneracion.Descripcion);
-                    tablaRemuneracionesBlancas.AddCell(remuneracion.Monto.ToString("C"));
-                    tablaRemuneracionesBlancas.AddCell(remuneracion.Fecha);
-                }
-
-
-                // Agregar tabla de retenciones
-                Table tablaRetenciones = new Table(4).UseAllAvailableWidth();
-
-                // Agregar celdas de encabezado a la tabla
-                tablaRetenciones.AddHeaderCell(codigoHeaderCell);
-                tablaRetenciones.AddHeaderCell(descripcionHeaderCell);
-                tablaRetenciones.AddHeaderCell(montoHeaderCell);
-                tablaRetenciones.AddHeaderCell(fechaHeaderCell);
-
-                foreach (RetencionDTO retencion in liquidacion.Items.Retenciones)
-                {
-                    tablaRetenciones.AddCell(retencion.Codigo);
-                    tablaRetenciones.AddCell(retencion.Descripcion);
-                    tablaRetenciones.AddCell(retencion.Monto.ToString("C"));
-                    tablaRetenciones.AddCell(retencion.Fecha.ToString("dd/MM/yyyy"));
-                }
-
-                // Agregar celdas de encabezado a la tabla
-                tablaRemuneracionesBlancas.AddHeaderCell(codigoHeaderCell);
-                tablaRemuneracionesBlancas.AddHeaderCell(descripcionHeaderCell);
-                tablaRemuneracionesBlancas.AddHeaderCell(montoHeaderCell);
-                tablaRemuneracionesBlancas.AddHeaderCell(fechaHeaderCell);
-
-                // Agregar tabla de remuneracion en negro
-                Table tablaRemuneracionNegro = new Table(4).UseAllAvailableWidth();
-                tablaRemuneracionNegro.AddHeaderCell(codigoHeaderCell);
-                tablaRemuneracionNegro.AddHeaderCell(descripcionHeaderCell);
-                tablaRemuneracionNegro.AddHeaderCell(montoHeaderCell);
-                tablaRemuneracionNegro.AddHeaderCell(fechaHeaderCell);
-
-                var listaRemuneracionNegro = liquidacion.Items.Remuneraciones.Where(r => r.EsBlanco == false);
-
-
-                foreach (RemuneracionDTO remuNegro in listaRemuneracionNegro)
-                {
-                    tablaRemuneracionNegro.AddCell(remuNegro.Codigo);
-                    tablaRemuneracionNegro.AddCell(remuNegro.Descripcion);
-                    tablaRemuneracionNegro.AddCell(remuNegro.Monto.ToString("C"));
-                    tablaRemuneracionNegro.AddCell(remuNegro.Fecha);
-
-                }
-
-                Table tablaPagarNegro = new Table(2).UseAllAvailableWidth();
-                Cell celdaDescripcion = new Cell().Add(new Paragraph("TOTAL A PAGAR EFECTIVO: ")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
-                Cell celdaMontoNegro = new Cell().Add(new Paragraph(liquidacion.TotalPagarEfectivo.ToString("C")));
-
-                tablaPagarNegro.AddHeaderCell(celdaDescripcion);
-                tablaPagarNegro.AddHeaderCell(celdaMontoNegro);
-
-                Table tablaPagarBlanco = new Table(2).UseAllAvailableWidth();
-                Cell celdaDescripcionB = new Cell().Add(new Paragraph("TOTAL A PAGAR EN BANCO: ")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
-                Cell celdaMontoBlanco = new Cell().Add(new Paragraph(liquidacion.TotalPagarBanco.ToString("C")));
-
-                tablaPagarBlanco.AddHeaderCell(celdaDescripcionB);
-                tablaPagarBlanco.AddHeaderCell(celdaMontoBlanco);
-
-
-                Paragraph subtituloRemuBlanca = new Paragraph("sueldo formal")
+                Paragraph subtituloRemuBlanca = new Paragraph("sueldo en banco")
                            .SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT)
                            .SetFontSize(14)
                            .SetBold();
 
-                Paragraph remuneracionSub = new Paragraph("Remuneraciones")
-                           .SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT)
-                           .SetFontSize(10)
-                           .SetBold();
 
-                Paragraph retenSub = new Paragraph("Retenciones")
-                           .SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT)
-                           .SetFontSize(10)
-                           .SetBold();
-
-                Paragraph subtituloRemuNegro = new Paragraph("sueldo informal")
+                Paragraph subtituloRemuNegro = new Paragraph("sueldo en efectivo")
                            .SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT)
                            .SetFontSize(14)
                            .SetBold();
+
+                Table tablaBlanco = this.GenerarTablaSueldoBlanco(liquidacion.Items.Retenciones, liquidacion.Items.Remuneraciones);
+                Table netoBlanco = this.GenerarTablaMontoNeto(false, liquidacion.TotalPagarBanco);
+                Table netoEfectivo = this.GenerarTablaMontoNeto(true, liquidacion.TotalPagarEfectivo);
+
+                Table tablaEfectivo = this.GenerarTablaSueldoEfectivo(liquidacion.Items.Descuentos, liquidacion.Items.Remuneraciones);
 
                 document.Add(subtituloRemuBlanca);
-                document.Add(remuneracionSub);
-                document.Add(tablaRemuneracionesBlancas);
+                document.Add(tablaBlanco);
                 document.Add(new Paragraph("\n"));
-                document.Add(retenSub);
-                document.Add(tablaRetenciones);
-                document.Add(new Paragraph("\n"));
-                document.Add(tablaPagarBlanco);
+                document.Add(netoBlanco);
 
                 document.Add(new Paragraph("\n"));
 
                 document.Add(subtituloRemuNegro);
-                document.Add(tablaRemuneracionNegro);
+                document.Add(tablaEfectivo);
+
+
                 document.Add(new Paragraph("\n"));
-                document.Add(tablaPagarNegro);
+                document.Add(netoEfectivo);
+
+
+                // Crear una tabla para las firmas
+                Table tablaFirmas = new Table(2).UseAllAvailableWidth();
+                tablaFirmas.SetHorizontalAlignment(HorizontalAlignment.CENTER);
+
+                // Agregar celdas para las firmas del empleado y el empleador
+                tablaFirmas.AddCell(new Cell().Add(new Paragraph("Firma del Empleado:")).SetTextAlignment(TextAlignment.LEFT));
+                tablaFirmas.AddCell(new Cell().Add(new Paragraph("Firma del Empleador:")).SetTextAlignment(TextAlignment.LEFT));
+
+                tablaFirmas.SetFixedPosition(document.GetLeftMargin(), document.GetBottomMargin(),
+                                            ps.GetWidth() - document.GetLeftMargin() - document.GetRightMargin());
+                document.Add(tablaFirmas);
 
                 // Cerrar el documento
                 document.Close();
 
                 return stream.ToArray();
             }
+
         }
+
+        private Table GenerarTablaSueldoBlanco(List<RetencionDTO> retenciones, List<RemuneracionDTO> remuneraciones)
+        {
+            Table tablaParteBlanco = new Table(4).UseAllAvailableWidth();
+
+            // Crear celdas de encabezado y establecer el color de fondo
+            Cell codigoHeaderCell = new Cell().Add(new Paragraph("cod. operacion")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+            Cell descripcionHeaderCell = new Cell().Add(new Paragraph("descripcion")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+            Cell fechaHeaderCell = new Cell().Add(new Paragraph("fecha")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+            Cell montoHeaderCell = new Cell().Add(new Paragraph("monto")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+
+            // Agregar celdas de encabezado a la tabla
+            tablaParteBlanco.AddHeaderCell(codigoHeaderCell);
+            tablaParteBlanco.AddHeaderCell(descripcionHeaderCell);
+            tablaParteBlanco.AddHeaderCell(fechaHeaderCell);
+            tablaParteBlanco.AddHeaderCell(montoHeaderCell);
+
+            var listaRemuneracionesBlanca = remuneraciones.Where(r => r.EsBlanco == true);
+            var listaRetenciones = retenciones;
+
+            foreach (RemuneracionDTO remuBlanca in listaRemuneracionesBlanca)
+            {
+                tablaParteBlanco.AddCell(remuBlanca.Codigo);
+                tablaParteBlanco.AddCell(remuBlanca.Descripcion);
+                tablaParteBlanco.AddCell(remuBlanca.Fecha);
+                tablaParteBlanco.AddCell(remuBlanca.Monto.ToString("C"));
+
+            }
+
+            foreach (RetencionDTO retencion in listaRetenciones)
+            {
+                tablaParteBlanco.AddCell(retencion.Codigo);
+                tablaParteBlanco.AddCell(retencion.Descripcion);
+                tablaParteBlanco.AddCell(retencion.Fecha.ToString("dd/MM/yyyy"));
+                tablaParteBlanco.AddCell($"-{retencion.Monto.ToString("C")}");
+
+            }
+
+            return tablaParteBlanco;
+        }
+
+        private Table GenerarTablaSueldoEfectivo(List<DescuentoDTO> descuentos, List<RemuneracionDTO> remuneraciones)
+        {
+            var listaRemuneracionesNegro = remuneraciones.Where(r => r.EsBlanco == false);
+            var listaDescuentos = descuentos;
+
+            Table tablaParteEfectivo = new Table(4).UseAllAvailableWidth();
+
+            // Crear celdas de encabezado y establecer el color de fondo
+            Cell codigoHeaderCell = new Cell().Add(new Paragraph("cod. operacion")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+            Cell descripcionHeaderCell = new Cell().Add(new Paragraph("descripcion")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+            Cell fechaHeaderCell = new Cell().Add(new Paragraph("fecha")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+            Cell montoHeaderCell = new Cell().Add(new Paragraph("monto")).SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+
+            // Agregar celdas de encabezado a la tabla
+            tablaParteEfectivo.AddHeaderCell(codigoHeaderCell);
+            tablaParteEfectivo.AddHeaderCell(descripcionHeaderCell);
+            tablaParteEfectivo.AddHeaderCell(fechaHeaderCell);
+            tablaParteEfectivo.AddHeaderCell(montoHeaderCell);
+
+            foreach (RemuneracionDTO remuEfectivo in listaRemuneracionesNegro)
+            {
+                tablaParteEfectivo.AddCell(remuEfectivo.Codigo);
+                tablaParteEfectivo.AddCell(remuEfectivo.Descripcion);
+                tablaParteEfectivo.AddCell(remuEfectivo.Fecha);
+                tablaParteEfectivo.AddCell(remuEfectivo.Monto.ToString("C"));
+            }
+
+            foreach (DescuentoDTO descuento in listaDescuentos)
+            {
+                tablaParteEfectivo.AddCell(descuento.Codigo);
+                tablaParteEfectivo.AddCell(descuento.Descripcion);
+                tablaParteEfectivo.AddCell(descuento.Fecha.ToString("dd/MM/yyyy"));
+                tablaParteEfectivo.AddCell(descuento.Monto.ToString("C"));
+            }
+
+            return tablaParteEfectivo;
+        }
+
+        private Table GenerarTablaMontoNeto(bool esEfecitvo, decimal monto)
+        {
+            string leyenda = esEfecitvo == true ? "EN EFECTIVO" : "EN EL BANCO";
+
+            Table tablaNeto = new Table(2).UseAllAvailableWidth();
+            Cell celdaDescripcion = new Cell().Add(new Paragraph($"TOTAL NETO A PAGAR {leyenda}: "))
+                                    .SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+
+            Cell celdaMontoNegro = new Cell().Add(new Paragraph(monto.ToString("C")));
+
+            tablaNeto.AddHeaderCell(celdaDescripcion);
+            tablaNeto.AddHeaderCell(celdaMontoNegro);
+
+            return tablaNeto;
+        }
+
+        private Table GenerarTablaHeader(LiquidacionDTO liquidacion)
+        {
+            string periodo = $"{liquidacion.Periodo.Inicio.ToString("dd/MM/yyyy")} - {liquidacion.Periodo.Fin.ToString("dd/MM/yyyy")}";
+
+            Table header = new Table(2).UseAllAvailableWidth();
+
+            // Crear celda para el título que abarca 2 columnas
+            Cell celdaTitulo = new Cell(1, 2).Add(new Paragraph("RECIBO DE SUELDO"))
+                                             .SetTextAlignment(TextAlignment.CENTER);
+
+            celdaTitulo.SetBackgroundColor(ColorConstants.LIGHT_GRAY);
+            header.AddHeaderCell(celdaTitulo);
+
+            // Agregar celdas a la tabla
+            header.AddHeaderCell(new Cell().Add(new Paragraph("apellido y nombre")));
+            header.AddHeaderCell(new Cell().Add(new Paragraph(liquidacion.Empleado)));
+
+            header.AddHeaderCell(new Cell().Add(new Paragraph("N° de documento")));
+            header.AddHeaderCell(new Cell().Add(new Paragraph(liquidacion.Dni)));
+
+            header.AddHeaderCell(new Cell().Add(new Paragraph("monto bruto banco")));
+            header.AddHeaderCell(new Cell().Add(new Paragraph(liquidacion.TotalBrutoBanco.ToString("C"))));
+
+            header.AddHeaderCell(new Cell().Add(new Paragraph("monto bruto efectivo")));
+            header.AddHeaderCell(new Cell().Add(new Paragraph(liquidacion.TotalBrutoEfectivo.ToString("C"))));
+
+            header.AddHeaderCell(new Cell().Add(new Paragraph("periodo liquidado")));
+            header.AddHeaderCell(new Cell().Add(new Paragraph(periodo)));
+
+            header.AddHeaderCell(new Cell().Add(new Paragraph("concepto")));
+            header.AddHeaderCell(new Cell().Add(new Paragraph(liquidacion.Concepto)));
+
+            return header;
+        }
+
     }
 }
