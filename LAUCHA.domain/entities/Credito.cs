186bedb833ff0231c4a97416eafbe0e293ba3a3c
Gabyx708 <matiascoria427@gmail.com>
2024-03-05 15:27:17 -0300

    borrado de cuotas y subcuotas , e inicia la creacion de los pagos de creditos

--- LAUCHA.domain/entities/Credito.cs
+++ LAUCHA.domain/entities/Credito.cs
@@ -10,11 +10,16 @@ namespace LAUCHA.domain.entities
     {
         public string CodigoCredito { get; set; } = null!;
         public decimal Monto { get; set; }
+        public decimal MontoPagado { get; set; }
         public DateTime FechaInicio { get; set; }
+        public bool SePagaQuincenal { get; set; }
+        public bool Suspendido { get; set; }
         public string Descripcion { get; set; } = null!;
         public string NumeroCuenta { get; set; } = null!;
         public Cuenta Cuenta { get; set; } = null!;
-        public ICollection<Cuota> Cuotas { get; set; } = null!;
+        public int CantidadCuotasOriginales { get; set; }
+        public int CantidadCuotasPagadas { get; set; }
+        public int CantidadCuotasFaltantes { get;set; }
         public int NumeroConcepto { get; set; }
         public Concepto Concepto { get; set; } = null!;
     }
---
+++ LAUCHA.domain/entities/PagoCredito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAUCHA.domain.entities
+{
+    public class PagoCredito
+    {
+        public string CodigoDescuento { get; set; } = null!;
+        public string CodigoCredito { get; set; } = null!;
+        public DateTime FechaPago {  get; set; }
+        public decimal Monto { get; set; }
+
+    }
+}
