1a1c3d9cb1b626f2e1b82c14b596beb98e52a062
Gabyx708 <matiascoria427@gmail.com>
2026-05-29 13:49:22 -0300

    se agrego la prueba de creacion de empleados tomando en cuenta el cuil

---
+++ LAUCHA.tests/Features/Empleados/CrearEmpleado/CrearEmpleadoHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using LAUCHA.application;
+using LAUCHA.application.Features.Empleados.CrearEmpleado;
+using LAUCHA.domain.Entities.Empleados;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LAUCHA.tests.Features.Empleados.CrearEmpleado
+{
+    public class CrearEmpleadoHandlerTests
+    {
+        [Fact]
+        public async Task DebeCrearEmpleado()
+        {
+            // Arrange
+
+            var services = new ServiceCollection();
+
+            services.AddApplicationServices();
+
+            services.AddSingleton<IEmpleadoRepository, EmpleadoFakeRepository>();
+
+            var provider = services.BuildServiceProvider();
+
+            var casoDeUso = provider.GetRequiredService<ICrearEmpleado>();
+
+            var request = new CrearEmpleadoRequest(
+                Dni: "12345678",
+                Cuil: "20123456783",
+                Nombre: "Marcelo",
+                Apellido: "Perez",
+                FechaIngreso: DateTime.Today,
+                FechaNacimiento: new DateTime(1980, 1, 1),
+                FechaAlta: DateTime.Today
+            );
+
+            // Act
+
+            var result = await casoDeUso.Crear(request);
+
+            // Assert
+
+            result.IsSuccess.Should().BeTrue();
+
+            result.Value.Dni.Should().Be("12345678");
+            result.Value.Nombre.Should().Be("Marcelo");
+            result.Value.Apellido.Should().Be("Perez");
+        }
+    }
+}
---
+++ LAUCHA.tests/Features/Empleados/EmpleadoFakeRepository.cs
@@ -0,0 +1,35 @@
+﻿using LAUCHA.domain.Entities.Empleados;
+
+namespace LAUCHA.tests.Features.Empleados
+{
+    internal class EmpleadoFakeRepository : IEmpleadoRepository
+    {
+        public List<Empleado> Empleados { get; } = [];
+
+        public Task<IEnumerable<Empleado>> FindByNameOrSurname(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Empleado>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Empleado?> GetByDni(string dni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(Empleado emp)
+        {
+            Empleados.Add(emp);
+            return Task.CompletedTask;
+        }
+
+        public Task<Empleado> Update(Empleado emp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
